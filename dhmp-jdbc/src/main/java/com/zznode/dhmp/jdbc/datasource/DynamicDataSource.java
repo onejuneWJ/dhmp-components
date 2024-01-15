3ef51ca687398d76976a4d6adc8aaa2d2c0276e2
王俊 <1595890874@qq.com>
2024-01-15 10:53:45 +0800

    [FIX] 修复动态数据源关闭时, 实际的数据源没有得到关闭指令的问题

--- dhmp-jdbc/src/main/java/com/zznode/dhmp/jdbc/datasource/DynamicDataSource.java
+++ dhmp-jdbc/src/main/java/com/zznode/dhmp/jdbc/datasource/DynamicDataSource.java
@@ -8,7 +8,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * @author 王俊
  * @date create in 2023/5/25 11:28
  */
-public class DynamicDataSource extends AbstractRoutingDataSource {
+public class DynamicDataSource extends AbstractRoutingDataSource implements AutoCloseable {
 
     public DynamicDataSource(DataSourceProvider dataSourceProvider) {
 
@@ -19,5 +19,20 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
         return DynamicDataSourceContextHolder.getDataSourceType();
+    }
+
+    @Override
+    public void close() throws Exception {
+        getResolvedDataSources()
+                .values()
+                .forEach(dataSource -> {
+                    if (dataSource instanceof AutoCloseable autoCloseable) {
+                        try {
+                            autoCloseable.close();
+                        } catch (Exception e) {
+                            logger.error("error close data source", e);
+                        }
+                    }
+                });
     }
 }
